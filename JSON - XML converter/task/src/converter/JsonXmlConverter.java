7b925eaeb30bcfd459df958562935e6edfed54a8
Андрей Каменский <rybinsk@mail.ru>
2021-11-22 21:26:11 +0300

    toStringAsXML and tests for it added

--- JSON - XML converter/task/src/converter/JsonXmlConverter.java
+++ JSON - XML converter/task/src/converter/JsonXmlConverter.java
@@ -1,7 +1,13 @@
 package converter;
 
+import converter.JSON.JsonElement;
+import converter.JSON.JsonElementType;
+import converter.JSON.JsonMap;
+import converter.JSON.JsonString;
 import converter.XML.XmlElement;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,13 +42,87 @@ public class JsonXmlConverter {
         return res.toString();
     }
 
+    public static String toStringAsXML(JsonElement element) throws IllegalArgumentException {
+        //todo: make smaller!!!
+        final String error_prefix = "JSON to XML converting error: ";
+        if (element == null) {
+            return "";
+        }
+        if (element.getType() != JsonElementType.MAP) {
+            throw new IllegalArgumentException(error_prefix + "element must be the MAP type");
+        }
+        Map<String, JsonElement> map = ((JsonMap) element).getMap();
+        if (map.size() != 1) {
+            throw new IllegalArgumentException(error_prefix + "more than one element in the map");
+        }
+        String xmlElementName = map.keySet().iterator().next();
+        Map<String, String> attr = null;
+        List<String> content = null;
+        JsonElement innerElement = map.get(xmlElementName);
+        JsonElementType innerType = innerElement.getType();
+        boolean hasContent = false;
+        boolean hasAttr = false;
+        if (innerType == JsonElementType.STRING) {
+            hasContent = true;
+            content = new ArrayList<>();
+            content.add(((JsonString)innerElement).getValue());
+        } else if (innerType == JsonElementType.MAP) {
+            Map<String, JsonElement> innerMap = ((JsonMap)innerElement).getMap();
+            String contentKeyName = "#" + xmlElementName;
+            for (Map.Entry<String, JsonElement> currentAttr : innerMap.entrySet()) {
+                if (currentAttr.getKey().startsWith(contentKeyName)) {
+                    if (currentAttr.getValue().getType() == JsonElementType.STRING) {
+                        hasContent = true;
+                        content = new ArrayList<>();
+                        content.add(((JsonString)currentAttr.getValue()).getValue());
+                    } else if (currentAttr.getValue().getType() == JsonElementType.NULL) {
+                        //do nothing
+                    } else if (currentAttr.getValue().getType() == JsonElementType.NUMBER) {
+                        content = new ArrayList<>();
+                        content.add(currentAttr.getValue().toString());
+                    } else {
+                        throw new IllegalArgumentException(error_prefix
+                                + "now accepts only string, null or number type content");
+                    }
+                } else if (currentAttr.getKey().startsWith("@")) {
+                    if (!hasAttr) {
+                        hasAttr = true;
+                        attr = new HashMap<>();
+                    }
+                    String attrName = currentAttr.getKey().substring(1);
+                    String attrValue = "";
+                    JsonElement attrValueElement = currentAttr.getValue();
+                    JsonElementType attrValueType = attrValueElement.getType();
+                    if (attrValueType == JsonElementType.STRING) {
+                        attrValue = ((JsonString) attrValueElement).getValue();
+                    } else if (attrValueType == JsonElementType.NULL ||
+                            attrValueType == JsonElementType.NUMBER) {
+                        attrValue = attrValueElement.toString();
+                    } else {
+                        throw new IllegalArgumentException(error_prefix + "illegal type of the attribute value");
+                    }
+                    attr.put(attrName, attrValue);
+                } else {
+                    throw new IllegalArgumentException(error_prefix + "unknown key type.");
+                }
+            }
+        } else if (innerType == JsonElementType.NULL) {
+        } else  if (innerType == JsonElementType.NUMBER) {
+            hasContent = true;
+            content = new ArrayList<>();
+            content.add(innerElement.toString());
+        } else {
+            throw new IllegalArgumentException(error_prefix + "illegal inner element type");
+        }
+        return new XmlElement(xmlElementName, attr, content).toString();
+    }
+
     private static void addJsonContent(StringBuilder res, XmlElement xmlElement) {
         if (xmlElement.hasContent() && !xmlElement.getContent().isEmpty()) {
             List<String> content = xmlElement.getContent();
             if (content.size() == 1) {
-                res.append("\"");
-                res.append(xmlElement.getContent().get(0));
-                res.append("\"\n");
+                res.append(JsonString.toJsonString(xmlElement.getContent().get(0)));
+                res.append("\n");
             } else {
                 res.append("[");
                 boolean first = true;
--- JSON - XML converter/task/test/converter/JsonXmlConverterTest.java
+++ JSON - XML converter/task/test/converter/JsonXmlConverterTest.java
@@ -1,5 +1,7 @@
 package converter;
 
+import converter.JSON.JsonElement;
+import converter.JSON.JsonElementLoader;
 import converter.XML.XmlElement;
 import junit.framework.TestCase;
 
@@ -53,4 +55,79 @@ public class JsonXmlConverterTest extends TestCase {
 
 
     }
+
+    public void checkJSONtoXML(String jsonIn, String xmlOut) {
+        JsonElement jsonElement = new JsonElementLoader(jsonIn).getElement();
+        String toXml = JsonXmlConverter.toStringAsXML(jsonElement);
+        System.out.println(toXml);
+        assert xmlOut.equals(toXml);
+    }
+
+    public void twoDimensionConverting(String jsonOrXml) {
+        StringIndex i = new StringIndex(0, jsonOrXml);
+        if (i.getNextNotSpaceChar() == '{') {
+            //json
+            JsonElement jsonElement = new JsonElementLoader(jsonOrXml).getElement();
+            String jsonString = jsonElement.toString();
+            System.out.println(jsonString);
+            String xmlString = JsonXmlConverter.toStringAsXML(jsonElement);
+            twoDimensionConverting(xmlString);
+        } else if (i.getCurrentChar() == '<') {
+            //xml
+            XmlElement xmlElement = new XmlElement(new StringIndex(0,jsonOrXml));
+            String xmlString = xmlElement.toString();
+            String jsonString = JsonXmlConverter.toStringAsJson(xmlElement);
+            JsonElement jsonElement = new JsonElementLoader(jsonString).getElement();
+            String jsonToXml = JsonXmlConverter.toStringAsXML(jsonElement);
+            System.out.println();
+            System.out.println(xmlString);
+            System.out.println(jsonString);
+            assert xmlString.equals(jsonToXml);
+        }
+    }
+
+    public void test_toStringAsXml() {
+        checkJSONtoXML("{\"jdk\" : \"1.8.9\"}", "<jdk>1.8.9</jdk>");
+        checkJSONtoXML("{\n" + "    \"employee\" : {\n" + "        \"@department\" : \"manager\",\n" +
+                        "        \"#employee\" : \"Garry Smith\"\n" + "    }\n" + "}",
+                "<employee department = \"manager\">Garry Smith</employee>");
+        checkJSONtoXML("{\n" +
+                "    \"person\" : {\n" +
+                "        \"@rate\" : 1,\n" +
+                "        \"@name\" : \"Torvalds\",\n" +
+                "        \"#person\" : null\n" +
+                "    }\n" +
+                "}", "<person rate = \"1\" name = \"Torvalds\" />");
+
+        checkJSONtoXML("{\"name\" : null }", "<name />");
+        checkJSONtoXML("{\"name\" : 1 }", "<name>1</name>");
+        checkJSONtoXML("{\"name\" : \"\" }", "<name></name>");
+    }
+
+    public void test_twoDimensions() {
+        twoDimensionConverting("<name/>");
+        twoDimensionConverting("{\"qwe\":\tnull}");
+        twoDimensionConverting("<qwerty at = \"1234\" name = \"kira\">KiraNa</qwerty>");
+        twoDimensionConverting("<qwerty at = \"1234\" name = \"kira\">\"KiraNa\"</qwerty>");
+        twoDimensionConverting("{\n" +
+                "  \"qwerty\" : {\n" +
+                "    \"@str\" : \"1234\",\n" +
+                "    \"@null\" : null,\n" +
+                "    \"@num\" : 123E55,\n" +
+                "    \"#qwerty\" : \"\\\"KiraNa\\\"\"\n" +
+                "  }\n" +
+                "}");
+        twoDimensionConverting("{\n" +
+                "  \"qwerty\" : {\n" +
+                "    \"@str\" : \"1234\",\n" +
+                "    \"@null\" : null,\n" +
+                "    \"@num\" : 123E55,\n" +
+                "    \"#qwerty\" : 50e5\n" +
+                "  }\n" +
+                "}");
+
+    }
+
+
+
 }
