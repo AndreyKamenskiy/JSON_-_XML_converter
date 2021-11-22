f2d7fece0af83eb4671c15eda882ab5d5e58ee40
Андрей Каменский <rybinsk@mail.ru>
2021-11-22 21:28:56 +0300

    rewrote to load from the file test.txt

--- JSON - XML converter/task/src/converter/Main.java
+++ JSON - XML converter/task/src/converter/Main.java
@@ -1,83 +1,38 @@
 
 package converter;
 
+import converter.JSON.JsonElement;
+import converter.JSON.JsonElementLoader;
+import converter.XML.XmlElement;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
-    static String convertJSONToXML(String input) {
-        StringBuilder result = new StringBuilder();
-
-        final String HAS_NULL = "\\s*\\{\\s*\".+\"\\s*:\\s*null\\s*}\\s*";
-        final String KEY_VALUE_JSON_ELEMENT = "\\s*\\{\\s*\"[^\"]+\"\\s*:\\s*\"[^\"]+\"\\s*}\\s*";
-
-        String value = "";
-        String key = "";
-        if (input.matches(HAS_NULL)) {
-            key = input.replaceFirst("\\s*\\{\\s*\"","")
-                    .replaceFirst("\\s*\".+","").trim();
-            result.append("<");
-            result.append(key);
-            result.append("/>");
-        } else if (input.matches(KEY_VALUE_JSON_ELEMENT)) {
-            key = input.replaceFirst("\\s*\\{\\s*\"","")
-                    .replaceFirst("\\s*\".+","").trim();
-            value = input.replaceFirst("\\s*\\{\\s*\"[^\"]+\"\\s*:\\s*\"","")
-                    .replaceFirst("\".+","");
-
-            result.append("<");
-            result.append(key);
-            result.append(">");
-            result.append(value);
-            result.append("</");
-            result.append(key);
-            result.append(">");
-        }
-        return result.toString();
-    }
-
-    static String convertXMLToJSON(String input) {
-        StringBuilder result = new StringBuilder();
-
-        final String SINGLE_ELEMENT = "\\s*<.*/>\\s*";
-        final String DOUBLE_ELEMENT = "\\s*<.+>.*<\\s*/.+>\\s*";
-
-        String value = "";
-        String key = "";
-        if (input.matches(SINGLE_ELEMENT)) {
-            key = input.replace("<","").replaceAll("/\\s*>\\s*","").trim();
-            value = "null";
-        } else if (input.matches(DOUBLE_ELEMENT)) {
-            key = input.replaceAll(">.*<\\s*/.+>\\s*","").replaceFirst("<\\s*","");
-            value = '"' + input.replaceAll("<[^>]*>", "") + '"';
-        }
-
-        result.append("{ \"");
-        result.append(key);
-        result.append("\" : ");
-        result.append(value);
-        result.append("}");
-
-        return result.toString();
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        // Check for format JSON or XML
-        final String JSON_FORMAT = "\\s*\\{.+}\\s*";
-        //final String XML_FORMAT = "\\s*\".+\"\\s*";///????
-        final String XML_FORMAT = "\\s*<.+>\\s*";
-
-        String result = "";
-
-        if (input.matches(JSON_FORMAT)) {
-            result = convertJSONToXML(input);
-        } else if (input.matches(XML_FORMAT)) {
-            result = convertXMLToJSON(input);
+        File inputFile = new File("test.txt");
+        Scanner scanner;
+        try {
+            scanner = new Scanner(inputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
         }
-        System.out.println(result);
-
+        String input = scanner.useDelimiter("\\z").next();
+        scanner.close();
+        StringIndex i = new StringIndex(0, input);
+        String res = "";
+        if (i.getNextNotSpaceChar() == '{') {
+            //json
+            JsonElement jsonElement = new JsonElementLoader(input).getElement();
+            res = JsonXmlConverter.toStringAsXML(jsonElement);
+        } else if (i.getCurrentChar() == '<') {
+            //xml
+            XmlElement xmlElement = new XmlElement(new StringIndex(0,input));
+            res = JsonXmlConverter.toStringAsJson(xmlElement);
+        }
+        System.out.println(res);
     }
 }
